0bef33cf93c74d78f5f0c978b8bdc385139e6f9b
sinsoku <sinsoku.listy@gmail.com>
2014-09-07 20:01:41 +0900

    Add specs

--- Gemfile
+++ Gemfile
@@ -49,7 +49,7 @@ end
 group :development, :test do
   gem 'rspec-rails', '~> 3.0.0'
   gem 'factory_girl_rails'
-  gem 'database_cleaner'
+  gem 'database_cleaner', github: 'bmabey/database_cleaner'
 end
 
 gem 'pg', group: [:production, :test]
--- spec/requests/api/dev/jobs_spec.rb
+++ spec/requests/api/dev/jobs_spec.rb
@@ -4,16 +4,29 @@ describe 'Jobs API' do
   let(:json) { JSON.parse response.body }
 
   before :all do
-    @company = create :company, name: '株式会社grooves', screen_name: 'grooves'
-    @job = create :job, company: @company
-  end
-  before { get '/api/dev/jobs' }
+    grooves = create :company, name: '株式会社grooves', screen_name: 'grooves'
+    create :job, company: grooves, published_at: '2014-09-07'
 
-  describe 'job keys' do
-    it { expect(json[0].keys).to eq ['id', 'title', 'raw_description', 'company'] }
+    heroku = create :company, name: 'Heroku', screen_name: 'heroku'
+    create :job, company: heroku, published_at: '2014-09-06'
   end
 
-  describe 'company keys' do
-    it { expect(json[0]['company'].keys).to eq ['name', 'screen_name'] }
+  context 'get all jobs' do
+    before { get '/api/dev/jobs' }
+
+    it { expect(json.size).to eq 2 }
+
+    describe 'job keys' do
+      it { expect(json[0].keys).to eq ['id', 'title', 'raw_description', 'company'] }
+    end
+
+    describe 'company keys' do
+      it { expect(json[0]['company'].keys).to eq ['name', 'screen_name'] }
+    end
+  end
+
+  context 'search params exists' do
+    before { get '/api/dev/jobs', q: {company_name_cont: 'grooves'} }
+    it { expect(json.size).to eq 1 }
   end
 end
