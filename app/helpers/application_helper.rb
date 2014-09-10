db4c5e32661ec38110be22f2ebfa5c8b52b92079
sinsoku <sinsoku.listy@gmail.com>
2014-09-11 00:52:10 +0900

    DRY

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def link_to_forkwell(html_options = nil)
+    link_to 'Forkwell', 'https://forkwell.com/', html_options
+  end
+
+  def link_to_forkwell_jobs(html_options = nil)
+    link_to 'Forkwell Jobs', 'https://jobs.forkwell.com/', html_options
+  end
+
+  def link_to_api_doc(name, options, html_options = nil)
+    link_to name, ['docs', 'api', *options].join('/'), html_options
+  end
 end
